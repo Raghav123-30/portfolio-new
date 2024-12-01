4724a7cc4fd3c65ab6d9d11580284f387968c153
Raghav123-30 <Raghavendrabhat2018@gmail.com>
2024-12-01 15:39:49 +0530

    Improved intro design

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 export default function App() {
   return (
     <div>
-      <div className="max-w-7xl mx-auto p-4">
-        <div className="min-h-screen grid place-content-center p-8 font-sans ">
+      <div className="max-w-3xl mx-auto p-4">
+        <div className="min-h-screen grid place-content-center p-8 font-sans">
           <motion.div
             initial={{
               opacity: 0.5,
@@ -21,13 +21,13 @@ export default function App() {
               stiffness: 200,
               damping: 20,
             }}
-            className="flex flex-col gap-8 "
+            className="flex flex-col gap-8"
           >
-            <p className="text-xl text-teal-400">Hello, my name is</p>
-            <h1 className="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-teal-600 to-teal-400 inline-block">
+            <p className="text-lg text-gray-600">Hi, I'm</p>
+            <h1 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-teal-600 via-blue-500 to-purple-600 mb-8">
               Raghavendra Bhat
             </h1>
-            <p className="leading-relaxed text-teal-200">
+            <p className="text-lg leading-relaxed text-gray-700 text-justify">
               I am an adaptable software engineer with experience in web and
               mobile development, as well as cloud solutions. With over 2 years
               of experience, I've developed and shipped cross-platform mobile
@@ -39,20 +39,24 @@ export default function App() {
               <br />
               <br />
               This is my{" "}
-              <span className="font-semibold text-teal-500 text-xl">
+              <span className="font-semibold text-teal-600 text-lg">
                 portfolio project
               </span>
               , currently under construction 🚧. More content will be added
               soon.
             </p>
-            <button className="px-8 py-4 border-2 border-teal-500 bg-transparent hover:bg-teal-500 hover:text-white ease-in-out transition-transform duration-200  hover:border-slate-900 focus:outline-none rounded-md font-bold w-fit">
+            <motion.button
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+              className="px-8  py-4 border-2 bg-transparent border-gray-800 hover:bg-teal-600 hover:text-white ease-in-out transition-transform duration-200 hover:border-teal-600 focus:outline-none rounded-md font-semibold shadow-sm hover:shadow-lg"
+            >
               Coming Soon
-            </button>
+            </motion.button>
           </motion.div>
         </div>
       </div>
-      <footer className="mt-auto p-8 text-teal-500 text-center  bg-slate-800/40">
-        <p>Designed and built by Raghavendra Bhat</p>
+      <footer className="mt-auto p-8 text-gray-900 text-center ">
+        <p className="font-medium">Designed and built by Raghavendra Bhat</p>
       </footer>
     </div>
   );
